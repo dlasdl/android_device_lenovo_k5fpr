d1ef781c755ef6b75e8eb2c33031f605cb8527b7
SahilSonar <sss.sonar2003@outlook.com>
2019-01-14 08:37:34 +0000

    Path of YGPS is changed

--- YGPS/Android.mk
+++ YGPS/Android.mk
@@ -1,14 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
+LOCAL_OVERRIDES_PACKAGES := CMUpdater CMBugReport ResurrectionOTA ResurrectionStats MagiskManager Camera2
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := YGPS
-#LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_CERTIFICATE := platform
+LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
 
